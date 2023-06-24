817846b0949da5ac2ff9c0f64adcdce6b099af6d
Nic Bolton <bolton21@uwindsor.ca>
2023-06-24 13:22:21 -0400

    updated plugin path for linux

--- src/java/JNIMethods.java
+++ src/java/JNIMethods.java
@@ -2,7 +2,7 @@ class JNIMethods {
     private static final String DLL_PATH = System.getProperty("user.dir")
             + "\\plugins\\CISSCO\\Calculate_Magnetic_Moment_3D_Native.dll";
     private static final String SO_PATH = System.getProperty("user.dir")
-            + "/plugins/CISSCO/Calculate_Magnetic_Moment_3D_Native.so";
+            + "/ImageJ/plugins/CISSCO/Calculate_Magnetic_Moment_3D_Native.so";
 
     static {
         Calculate_Magnetic_Moment_3D.logger = new LogManager();
